37dfc8240a3c7f88a16671cb7325a55eacd64d48
Amit Singh <amitsingh19975@gmail.com>
2024-03-06 01:39:58 +0530

    Add utf8_len_after_skip method to ByteToCharIter

--- src/eoc/lexer/str_utils.rs
+++ src/eoc/lexer/str_utils.rs
@@ -6,6 +6,19 @@ impl<'a> ByteToCharIter<'a> {
     pub fn new(bytes: &'a [u8]) -> Self {
         ByteToCharIter(bytes)
     }
+
+    pub(crate) fn utf8_len_after_skip(&self, mut n: usize) -> usize {
+        let mut size = 0usize;
+        let mut iter = ByteToCharIter::new(self.0);
+        while let Some(c) = iter.next() {
+            if n == 0 {
+                break;
+            }
+            size += c.len_utf8();
+            n -= 1;
+        }
+        size
+    }
 }
 
 impl<'a> Iterator for ByteToCharIter<'a> {
