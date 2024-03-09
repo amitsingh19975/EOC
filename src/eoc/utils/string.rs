676f6ada7d68969c3ded526201874cbe67b68b4c
Amit Singh <amitsingh19975@gmail.com>
2024-03-09 13:48:37 +0530

    use hash set to improve find search speed

--- src/eoc/utils/string.rs
+++ src/eoc/utils/string.rs
@@ -1,14 +1,14 @@
 
-use std::{fmt::Display, sync::RwLock};
+use std::{collections::HashSet, fmt::Display, sync::RwLock};
 
 use lazy_static::lazy_static;
 
 lazy_static! {
-    static ref STRING_INTERNER: RwLock<Vec<&'static str>> = RwLock::new(Vec::new());
+    static ref STRING_INTERNER: RwLock<HashSet<&'static str>> = RwLock::new(HashSet::new());
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
-pub(crate) struct UniqueString(u32, &'static str);
+pub(crate) struct UniqueString(&'static str);
 
 impl Display for UniqueString {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -18,31 +18,23 @@ impl Display for UniqueString {
 
 impl UniqueString {
     pub(crate) fn new<S: AsRef<str>>(s: S) -> Self {
-        let index = Self::find_index(s.as_ref());
-        if index >= 0 {
-            let reader = STRING_INTERNER.write().unwrap();
-            return Self(index as u32, reader[index as usize])
+        let c_str = Self::get(s.as_ref());
+        if let Some(c_str) = c_str {
+            return Self(c_str)
         }
 
         let mut writer = STRING_INTERNER.write().unwrap();
-        let id = writer.len();
         let s = Box::leak(s.as_ref().to_owned().into_boxed_str());
-        writer.push(s);
-        Self(id as u32, s)
+        writer.insert(s);
+        Self(s)
     }
 
-    fn find_index(s: &str) -> isize {
+    fn get(s: &str) -> Option<&'static str> {
         let reader = STRING_INTERNER.write().unwrap();
-        for i in 0..reader.len() {
-            if reader[i] == s {
-                return i as isize;
-            }
-        }
-
-        -1
+        reader.get(s).copied()
     }
 
     pub(crate) fn as_str(&self) -> &'static str {
-        self.1
+        self.0
     }
 }
